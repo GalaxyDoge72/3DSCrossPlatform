28540e1deb48078fa8cbc286a066ab2a4ff680f4
galaxydoge72 <braydensucc@gmail.com>
2025-10-25 07:56:27 +1000

    for fucks sake please just work

--- source/crc.c
+++ source/crc.c
@@ -1,3 +1,21 @@
 #include "CRC.h"
+// No need for externs here, C++ compiler is the one building the function. //
 
-uint32_t 
+uint32_t calcCRC32(const uint8_t *data, size_t length) {
+    uint32_t crc = 0xFFFFFFFF;
+
+    for (size_t i = 0; i < length; i++) {
+        // XOR the current byte with the LSB of the CRC //
+        crc ^= data[i];
+
+        for (int j = 0; j < 8; j++) {
+            if (crc & 1) {
+                crc = (crc >> 1) ^ CRC32_POLYNOMAL;
+            }
+            else {
+                crc >>= 1;
+            }
+        }
+    }
+    return ~crc;
+}
---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+// C++ Standard Libraries //
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+// 3DS DevKitPro Libraries //
+#include <3ds.h>
+
+
+// Custom Libraries //
+#include "CRC.h"
+
+int main() {
+    
+}
